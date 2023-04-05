10df6164576a82289b8d11c3572679ea6ec9d03c
khairallah17 <med.khairallah3@gmail.com>
2023-04-05 06:16:34 +0000

    api backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.use(cors({
+    origin: "*"
+}))
+
+app.get("/", (req, res) => {
+    res.send("API WORKING")
+})
+
+app.get("/api", async (req, res) => {
+
+    const result = await axios(({
+        method: 'get',
+        url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'X-CMC_PRO_API_KEY': process.env.API_KEY
+        }    
+      }))
+
+      console.log("HERE")
+
+      res.send(result.data.data)
+
+})
+
+app.listen(PORT, () => console.log(`litening on port ${PORT}`))
